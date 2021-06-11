229a41d09ee2fdebe9019555b6529ece542c9368
Kremmeke <karim.lkasumi@gmail.com>
2021-06-11 14:10:18 +0200

    Added OrderService

---
+++ Food4Share.ServiceLayer/OrderService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food4Share.ServiceLayer
+{
+    class OrderService
+    {
+        public void PlaceHolder()
+        {
+
+        }
+    }
+}
