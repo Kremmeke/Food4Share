123b8cba107ca36194849345180ece1ded56057a
Kremmeke <karim.lkasumi@gmail.com>
2021-06-11 12:49:50 +0200

    Add project files.

---
+++ Food4Share.Domain/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food4Share.Domain
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int MenuItemId { get; set; }
+        public MenuItem MenuItem { get; set; }
+        public int Quantity { get; set; }
+    }
+}
